5f00993c5f2d099f30856d2f27238e5ed5526484
THE MD FAZIL <themdfazil.0000@gmail.com>
2026-07-25 12:50:51 +0530

    update db config with ssl and ipv4

--- db.js
+++ db.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Cloud database ke liye zaroori hai
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
+
 
 pool.connect()
   .then(() => console.log("✅ Database Connected"))
